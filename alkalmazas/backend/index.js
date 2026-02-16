fe5dca4de8c13c68f6b7a06a53850dee33d9bfee
Gál Gergő Károly <h260678@stud.u-szeged.hu>
2026-02-16 10:04:20 +0100

    port javitas

--- alkalmazas/backend/index.js
+++ alkalmazas/backend/index.js
@@ -1,7 +1,4 @@
 import app from './src/app.js';
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
